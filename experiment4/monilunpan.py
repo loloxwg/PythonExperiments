6ea1f26b1afae66184e5bf39c331e21884e7f760
loloxwg <loloxwg@outlook.com>
2020-11-09 23:54:44 +0800

    fix

--- experiment4/monilunpan.py
+++ experiment4/monilunpan.py
@@ -1,32 +1,32 @@
 # 模拟轮盘抽奖游戏
 import random
-
-rewardDict = {
-    '一等奖': {0, 0.08},
-    '二等奖': {0.08, 0.3},
-    '三等奖': {0.3, 1.0}
-}
-
-
-def rewardFun():
-    num = random.random()
-
-    for k, v in rewardDict.items():
-        if v[0] <= num < v[1]:
-            return k
-
-
-resultDict = {}
-
-for i in range(1000):
-    res = rewardFun()
-    if res in resultDict:
-        resultDict[res] = +1
-    else:
-        resultDict[res] = 1
-
-for k, v in rewardDict.items():
-    print(k, '-->', v)
+#
+# rewardDict = {
+#     '一等奖': {0, 0.08},
+#     '二等奖': {0.08, 0.3},
+#     '三等奖': {0.3, 1.0}
+# }
+#
+#
+# def rewardFun():
+#     num = random.random()
+#
+#     for k, v in rewardDict.items():
+#         if v[0] <= num < v[1]:
+#             return k
+#
+#
+# resultDict = {}
+#
+# for i in range(1000):
+#     res = rewardFun()
+#     if res in resultDict:
+#         resultDict[res] = +1
+#     else:
+#         resultDict[res] = 1
+#
+# for k, v in rewardDict.items():
+#     print(k, '-->', v)
 import random
 
 rewardDict = {
