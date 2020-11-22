dad6e6e8776e3971742b2bb9e97e2ed9f13179bb
loloxwg <loloxwg@outlook.com>
2020-11-22 15:19:43 +0800

    ex4 fix

--- experiment4/monilunpan.py
+++ experiment4/monilunpan.py
@@ -29,29 +29,20 @@ import random
 #     print(k, '-->', v)
 # 模拟轮盘抽奖游戏
 import random
-rewardDict = {
-    '一等奖': (0, 0.05),
-    '二等奖': (0.05, 0.45),
-    '三等奖': (0.45, 1.0)
-}
-
-
+rewardDict = {'一等奖': (0, 0.05),'二等奖': (0.05, 0.45),'三等奖': (0.45, 1.0)}
 def rewardFun():
-    """用户得奖等级"""
     # 生成一个0～1之间的随机数
     num = random.random()
     # 判断随机转盘转的是几等奖
     for k, v in rewardDict.items():
         if v[0] <= num < v[1]:
             return k
-
 resultDict = {}
 for i in range(1000):
-    res = rewardFun()
-    if res not in resultDict:
-        resultDict[res] = 1
+    a = rewardFun()
+    if a not in resultDict:
+        resultDict[a] = 1
     else:
-        resultDict[res] += 1
-
+        resultDict[a] += 1
 for k, v in resultDict.items():
     print(k, '------>', v)
