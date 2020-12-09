1ba1445cc447bc1e37d665892ab8c8699b3be06b
loloxwg <loloxwg@outlook.com>
2020-12-09 22:26:25 +0800

    some fix

--- test/homework/9-2.py
+++ test/homework/9-2.py
@@ -3,11 +3,9 @@ import pickle
 dic = {'TOM': 60, 'JERRY': 100, 'ALEX': 89, 'THUMP': 44}
 filename = 'sc.dat'
 
-
 with open(filename, 'wb')as file_object:
     pickle.dump(dic, file_object)
 
 with open(filename, 'rb')as file_object:
-    dict=pickle.load(file_object)
-    print(dict)
-
+    dicts = pickle.load(file_object)
+    print(dicts)
